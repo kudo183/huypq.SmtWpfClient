6422fc77faf49798f069a9f0befc9a2240899139
kudo183@gmail.com <kudo183@gmail.com>
2017-05-05 14:53:39 +0700

    - add AllView.

---
+++ huypq.SmtWpfClient/View/AllView.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace huypq.SmtWpfClient.View
+{
+    /// <summary>
+    /// Interaction logic for AllView.xaml
+    /// </summary>
+    public partial class AllView : UserControl
+    {
+        private Dictionary<string, UserControl> _views = new Dictionary<string, UserControl>();
+        public AllView()
+        {
+            InitializeComponent();
+
+            var viewTypes = System.Reflection.Assembly.GetEntryAssembly().GetTypes().ToList();
+
+            foreach (var viewType in viewTypes.Where(p => p.Namespace.EndsWith("View") && p.DeclaringType == null).OrderBy(p => p.Name))
+            {
+                sp.Children.Add(new Button()
+                {
+                    Content = viewType.Name,
+                    Tag = viewType,
+                    HorizontalContentAlignment = HorizontalAlignment.Left
+                });
+            }
+
+            foreach (var viewType in viewTypes.Where(p => p.Namespace.EndsWith("View.Report") && p.DeclaringType == null).OrderBy(p => p.Name))
+            {
+                sp.Children.Add(new Button()
+                {
+                    Content = viewType.Name,
+                    Tag = viewType,
+                    HorizontalContentAlignment = HorizontalAlignment.Left
+                });
+            }
+
+        }
+
+        private void StackPanel_Click(object sender, RoutedEventArgs e)
+        {
+            var button = (e.OriginalSource as Button);
+            if (button == null)
+            {
+                return;
+            }
+
+            var viewType = button.Tag as System.Type;
+            var typeName = viewType.Name;
+            if (_views.ContainsKey(typeName) == false)
+            {
+                _views[typeName] = System.Activator.CreateInstance(viewType) as UserControl;
+            }
+
+            brd.Child = _views[typeName];
+        }
+    }
+}
